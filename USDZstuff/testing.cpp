a6f8c83c6376797ffe09152476ba25486b7e269b
sethlenhof <setleelen@gmail.com>
2024-09-01 22:20:06 -0400

    Add project files.

---
+++ USDZstuff/testing.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+#include <cmath>
+
+int main(int argc, char* argv[]) {
+	
+	
+	std::cout << "Hello world!" << std::endl;
+	double value = 1.0;
+	double result = std::sin(value);
+
+	std::cout << "The sin of " << value << " is " << result << std::endl;
+
+
+}
